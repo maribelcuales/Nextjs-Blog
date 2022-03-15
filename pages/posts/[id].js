78ec264e52eade102d34cd6049485b94f81aca41
maribelcuales <maribelcuales@gmail.com>
2022-03-15 19:26:24 +0800

    Update Post component to render contentHtml.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -18,6 +18,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
