9c20ddfa1888cc73fc7d95d8625d3c58b3f1fafc
maribelcuales <maribelcuales@gmail.com>
2022-03-16 22:06:53 +0800

    Import CSS styling from utils module.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
-import Head from 'next/head'; 
 import Layout from "../../components/layout";
+import Head from "next/head";
+import Date from "../../components/date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticPaths() {
   // Return a list of possible value for id
@@ -17,19 +19,20 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
 
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
