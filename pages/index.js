b568f0429ea73347a7899447fe4b0a5a5429d195
maribelcuales <maribelcuales@gmail.com>
2022-03-17 13:57:50 +0800

    Added href to Link.

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link>
+              <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
