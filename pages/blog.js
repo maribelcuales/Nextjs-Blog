ee4139163cd5e21789585d6d2444064aa5d71cd5
maribelcuales <maribelcuales@gmail.com>
2022-03-20 21:16:44 +0800

    Added getStaticProps() that calls an external API endpoint to get posts.

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,8 @@
-import { loadPosts } from '../lib/load-posts';
+import { loadPosts } from "../lib/load-posts";
 
-// This function runs only on the server side 
+// This function runs only on the server side
 
-// posts will be populated at build time by getStaticProps() 
+// posts will be populated at build time by getStaticProps()
 function Blog({ posts }) {
   return (
     <ul>
@@ -10,5 +10,23 @@ function Blog({ posts }) {
         <li>{post.title}</li>
       ))}
     </ul>
-  )
+  );
 }
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do direct database queries.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  // Can use any data fetching library
+  const res = await fetch("https://.../posts");
+  const posts = await res.json();
+
+  // By returning { posts: { posts } }, the Blog component will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Blog;
