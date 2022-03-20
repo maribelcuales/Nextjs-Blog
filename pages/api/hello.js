29820e8842cf6dab4635ceee1188bd324bc5524b
maribelcuales <maribelcuales@gmail.com>
2022-03-20 20:54:25 +0800

    Write initial code for formHandler function to save email to database.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,3 +2,9 @@
 export default function handler(req, res) {
   res.status(200).json({ text: "Hello" });
 }
+
+
+export default function formHandler(req, res) {
+  const email = req.body.email 
+  // Then save email to your database, etc. 
+}
